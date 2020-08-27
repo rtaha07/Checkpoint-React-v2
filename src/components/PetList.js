d942fa99f52e14dfe345a52a9c6a3cefe0ac2726
rtaha07 <rtaha07@gmail.com>
2020-08-27 00:36:55 -0400

    Finished Tier1

--- src/components/PetList.js
+++ src/components/PetList.js
@@ -1,23 +1,56 @@
 import React from 'react';
 import SinglePet from './SinglePet';
 
-const cody = {
-  id: 2,
-  name: 'Cody',
-  description: 'Adorable pug who loves to hug',
-  species: 'dog',
-};
-
-// PetList only renders one SinglePet. We'd like it to render a list of pets,
-// passed in as props.pets. Don't forget to add a unique key to each one!
 class PetList extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      filter: 'all',
+    };
+  }
+
   render() {
+    const { filter } = this.state;
+    const { handleDelete } = this.props;
+    const { pets } = this.props;
+
+    const filteredPets = pets.filter((pet) => {
+      switch (filter) {
+        case 'all':
+          return pet;
+        case 'cats':
+          pet.species === 'cat';
+          break;
+        case 'dogs':
+          pet.species === 'dog';
+          break;
+      }
+    });
+
     return (
-      <>
-        <div className="pet-list">
-          <SinglePet pet={cody} />
+      <div>
+        <div>
+          <label htmlFor="speciesFilter">Filter by species: </label>
+          <select
+            onChange={(event) => {
+              this.setState({ filter: event.target.value });
+            }}
+            value={filter}
+            id="petId"
+          >
+            <option value="all">all</option>
+            <option value="cats">cats</option>
+            <option value="dogs">dogs</option>
+          </select>
         </div>
-      </>
+        <div className="pet-list">
+          {filteredPets.map((pet) => {
+            return (
+              <SinglePet handleDelete={handleDelete} key={pet.id} pet={pet} />
+            );
+          })}
+        </div>
+      </div>
     );
   }
 }
--- src/components/SinglePet.js
+++ src/components/SinglePet.js
@@ -1,8 +1,34 @@
 import React from 'react';
+import DeletePet from './DeletePet';
 
 class SinglePet extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      adopted: false,
+    };
+  }
+
   render() {
-    return <div className="single-pet"></div>;
+    const { pet } = this.props;
+    const { handleDelete } = this.props;
+    const { adopted } = this.state;
+
+    return (
+      <div className={adopted ? 'single-pet adopted' : 'single-pet'}>
+        <h4>Name: {pet.name}</h4>
+        <div>Description: {pet.description}</div>
+        <div>Species: {pet.species}</div>
+        <br />
+        <div>Status: {adopted ? 'Adopted!' : 'Available for adoption'}</div>
+        <br />
+        <button onClick={() => this.setState({ adopted: !adopted })}>
+          Toggle Status
+        </button>
+        <br />
+        <DeletePet handleDelete={handleDelete} petId={pet.id} />
+      </div>
+    );
   }
 }
 
